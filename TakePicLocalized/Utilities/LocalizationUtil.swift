241e2eed6c613507316062b40b39328edd08dc11
Ashish <ashish@drillmaps.com>
2020-11-19 19:25:34 -0800

    Checking in code for localization and Taking pictures

---
+++ TakePicLocalized/Utilities/LocalizationUtil.swift
@@ -0,0 +1,11 @@
+//
+//  LocalizationUtil.swift
+//  TakePicLocalized
+//
+//  Created by Ashish Ashish on 11/19/20.
+//
+
+import Foundation
+
+var strTakePic = NSLocalizedString("take_pic", comment: "")
+
